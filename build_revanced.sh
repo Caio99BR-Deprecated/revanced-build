7476845cac3e7a7c24fc1b762b26d8fe67c503a2
Caio Oliveira <caiooliveirafarias0@gmail.com>
2022-07-24 12:51:18 -0300

    build_revanced.sh: remove upload direct to Telegram

--- build_revanced.sh
+++ build_revanced.sh
@@ -94,9 +94,6 @@ if [ -f "com.google.android.youtube.apk" ]; then
         ${patches[@]} \
         $EXPERIMENTAL \
         -a com.google.android.youtube.apk -o build/revanced-nonroot.apk
-	zip build/revanced-nonroot.zip build/revanced-nonroot.apk
-	zip build/revanced-nonroot.zip --out build/revanced-split.zip -s 45m
-	curl -F document=@"build/revanced-split.z*" "https://api.telegram.org/bot${TELEGRAM_BOT_API}/sendDocument?chat_id=${TELEGRAM_CHAT_ID}"
 else
     echo "Cannot find YouTube APK, skipping build"
 fi
@@ -110,7 +107,6 @@ if [ -f "com.google.android.apps.youtube.music.apk" ]; then
         ${patches[@]} \
         $EXPERIMENTAL \
         -a com.google.android.apps.youtube.music.apk -o build/revanced-music-nonroot.apk
-	curl -F document=@"build/revanced-music-nonroot.apk" "https://api.telegram.org/bot${TELEGRAM_BOT_API}/sendDocument?chat_id=${TELEGRAM_CHAT_ID}"
 else
     echo "Cannot find YouTube Music APK, skipping build"
 fi
